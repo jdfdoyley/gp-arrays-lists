ad5fc9d3949486cd5ae418a777bf2a97ce7e8dcf
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-20 04:33:25 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/******************************************************************************\
+* Name: Jason D. F. D'Oyley
+* Date: October 20, 2022
+* Assignment CIS214 Week 3 GP - Arrays and Lists
+*
+* Main application (program) class.
+* In this application we will demonstrate the use of Arrays and Lists for
+* holding lists of items. The special for loop is also introduce as a looping
+* mechanism useful when working with arrays and loops are only used with
+* collections of things, like Arrays and Lists.
+\******************************************************************************/
+public class App
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Arrays and Lists
+
+In this application I will demonstrate the use of Arrays and Lists for holding
+lists of items. The special for loop is also introduced as a looping mechanism
+useful when working with Arrays and Lists; commonly called a "for-each" loop.
+Note that for-each loops are only used with collections of things, like Arrays
+and Lists.
